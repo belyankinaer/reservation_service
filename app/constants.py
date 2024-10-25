ae496b241b8e084c93fd902342782783854415ad
belyankinaer <belyankinaer@soft-division.ru>
2024-10-25 11:12:30 +0300

    fix: рефакторинг

--- app/constants.py
+++ app/constants.py
@@ -3,11 +3,11 @@ import os
 SERVICE_HOST = os.getenv('SERVICE_HOST', '0.0.0.0')
 SERVICE_PORT = int(os.getenv('SERVICE_PORT', 8000))
 
-DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_HOST = os.getenv('DB_HOST', 'your_host')
 DB_PORT = int(os.getenv('DB_PORT', 5432))
 DB_NAME = os.getenv('DB_NAME', 'reservation_service')
-DB_PASS = os.getenv('DB_PASS', 'admin')
-DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASS = os.getenv('DB_PASS', 'your_password')
+DB_USER = os.getenv('DB_USER', 'your_user')
 
 # Формирование URL для базы данных
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
